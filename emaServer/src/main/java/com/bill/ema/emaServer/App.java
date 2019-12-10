0b85c3e0db7514da9429dae563001de12a3c4773
billlang <lang393386920@qq.com>
2019-12-10 18:01:55 +0800

    1.create project.

---
+++ emaServer/src/main/java/com/bill/ema/emaServer/App.java
@@ -0,0 +1,13 @@
+package com.bill.ema.emaServer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
